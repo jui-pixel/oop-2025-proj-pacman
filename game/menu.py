7df72fe1c3bcb45bbc7f07a3476243e03a9b86cb
charles691 <kiki.ee13@nycu.edu.tw>
2025-06-13 18:27:45 +0800

    新增迴圈以支援背景繪製的細節

--- game/menu.py
+++ game/menu.py
@@ -19,6 +19,7 @@ except ImportError:
     CUDA_DEVICE = "N/A"
 BACKGROUND = pygame.Surface((500, 500))
 BACKGROUND.fill(DARK_GRAY_BLUE)
+for x in range(0, 500, 10):
 class MenuButton:
     """
     定義選單按鈕類，包含位置、文字和樣式。
